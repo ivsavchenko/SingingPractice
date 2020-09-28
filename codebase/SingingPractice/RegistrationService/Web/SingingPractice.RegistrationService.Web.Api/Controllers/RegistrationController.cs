b340d2936018a09e2a4591aec02d0091cdcadb5a
ivsavchenko <ivsavchenko>
2020-09-28 03:34:37 +0200

    solution init

---
+++ codebase/SingingPractice/RegistrationService/Web/SingingPractice.RegistrationService.Web.Api/Controllers/RegistrationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SingingPractice.RegistrationService.Web.Api.Controllers
+{
+    [Route("api/registrations")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly ILogger<RegistrationController> logger;
+
+        public RegistrationController(ILogger<RegistrationController> logger)
+        {
+            this.logger = logger;
+        }
+
+#warning only for testing
+        [HttpGet]
+        public IActionResult HelloWorld()
+        {
+            logger.LogInformation($"HelloWorld web method was called at {DateTime.UtcNow:O}");
+            return Ok("Hello World!");
+        }
+    }
+}
